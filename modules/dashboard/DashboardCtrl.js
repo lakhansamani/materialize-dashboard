f0e4d94a5b757078a9c6b7eb5450a05b07002ecc
lakhansamani <lakhan.m.samani@gmail.com>
2016-02-20 15:12:04 +0530

    Modifies dashboard

--- modules/dashboard/DashboardCtrl.js
+++ modules/dashboard/DashboardCtrl.js
@@ -5,4 +5,7 @@ app.controller('DashboardCtrl',['$state','$scope',function($state,$scope){
 		[65, 59, 80, 81, 56, 55, 40],
 		[28, 48, 40, 19, 86, 27, 90]
 	];
+	$scope.labelsPie = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
+	$scope.dataPie = [300, 500, 100];
+
 }]);
